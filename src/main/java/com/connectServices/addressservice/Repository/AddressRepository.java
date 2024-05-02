d7f3e9ef9bc37d54fc1fcfe4164c682d7df71488
kapilan1 <kapilan@codelantic.com>
2024-05-02 16:51:53 +0530

    test cases added

--- src/main/java/com/connectServices/addressservice/Repository/AddressRepository.java
+++ src/main/java/com/connectServices/addressservice/Repository/AddressRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends JpaRepository<Address,Integer> {
 
+            // here can't use employeeId directly, the named parameter of employeeId, which is currentEmpId can be use
     @Query(nativeQuery = true,value = "SELECT  ea.id,ea.first_lane,ea.last_lane,ea.state,ea.zip FROM  employee_service.address ea join  employee_service.employee em on em.id = ea.employee_id where ea.employee_id = :currentEmpId")
     Address findAddressByEmployeeId(@Param("currentEmpId") int employeeId);
 }
